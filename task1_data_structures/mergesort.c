871f106bb5183497786172f45d7a313aa9486b64
colinaaa <qingyu.wang@aliyun.com>
2018-11-01 20:41:03 +0000

    mergesort

--- task1_data_structures/mergesort.c
+++ task1_data_structures/mergesort.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-void mergesort(int a[], int first, int last)
+void merge_2_array(int a[],int first_index1, int last_index1, int last_index2, int tem[]);
+
+void mergesort(int a[], int first, int last, int tem[])
 {
 		if (first<last)
 		{
-				int n=last-first+1;
-				int last_index1=first+n/2,first_index2=last_index1+1;
-				mergesort(a,first,last_index1);
-				mergesort(a,first_index2,last);
-				merge_2_array(a,first,last_index1,last);
+				int last_index1=(first+last)/2,first_index2=last_index1+1;
+				mergesort(a,first,last_index1,tem);
+				mergesort(a,first_index2,last,tem);
+				merge_2_array(a,first,last_index1,last,tem);
 		}
 }
 
-void merge_2_array(int a[], int first_index1,int last_index1, int last_index2)
+void merge_2_array(int a[], int first_index1,int last_index1, int last_index2, int tem[])
 {
 		//我意识到错误了。。。first_index2=last_index1+1。。。但是懒得改了。。。
 		int index_t=0;
 		int n=last_index2-first_index1+1;//两个数组的总个数
-		int tem[n];//创建一个临时储存数组
 		int first_index2=last_index1+1;//我还是改过来了。。
 		int tem_first_index1=first_index1;
 		int i;
@@ -51,7 +51,7 @@ void merge_2_array(int a[], int first_index1,int last_index1, int last_index2)
 				index_t++;
 		}
 
-		for(i=0;i<n;i++)//将排序好的数组copy到原数组中
+		for(i=0;i<index_t;i++)//将排序好的数组copy到原数组中
 		{
 				a[tem_first_index1+i]=tem[i];
 		}
@@ -61,6 +61,7 @@ int main(void)
 {
 		int a[9]={4,2,9,5,1,7,3,6,8};
 		int n=9;
+		int tem[9];
 		printf("now we have an array with %d integer.\n",n);
 		printf("it hasn't been sorted yet. \n");
 		int i;
@@ -70,7 +71,7 @@ int main(void)
 		}
 		putchar('\n');
 		printf("now let's sort it with mergesort\n");
-		mergesort(a,0,n-1);
+		mergesort(a,0,n-1,tem);
 		printf("the array has been sorted\n");
 
 		for ( i=0;i<n;i++)
