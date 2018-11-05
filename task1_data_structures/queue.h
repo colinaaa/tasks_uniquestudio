231a0ba5df8e58432fa750cc6578b606716ce575
colinaaa <qingyu.wang@aliyun.com>
2018-11-05 18:30:26 +0000

    new begin

---
+++ task1_data_structures/queue.h
@@ -0,0 +1,29 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+struct queue_str;
+typedef struct queue_str Queue;
+typedef unsigned int Index;
+
+struct queue_str
+{
+		Index ahead;
+		Index behind;
+		int * array;
+		int size;
+};
+
+void in_queue(Queue Q, int x)
+{
+		int tem_index;
+		tem_index=++Q->behind;
+		if (tem_index>=Q->size)
+		{
+				tem_index-=size;
+		}
+		Q->array[tem_index]=x;
+		
+}
+
+
+#endif
