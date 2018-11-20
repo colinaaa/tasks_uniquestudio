eb644547399b3aa84be42619f397cf4d8ecc915c
colinaaa <qingyu.wang@aliyun.com>
2018-11-20 23:06:07 +0000

    feat(http_server): implement a new class ListenSocket derived from Socket
    BREAKING CHANGE: use public inheritance to create a class learn a lot about cpp

--- task3_http_server/server.cpp
+++ task3_http_server/server.cpp
@@ -2,9 +2,7 @@
 #include<iostream>
 using namespace std;
 
-Socket::Socket()
-{
-}
+Socket::Socket(int fd, struct sockaddr_in* addr):socketfd(fd), sock_addr(*addr){}//有fd和地址的构造函数(for connect socket)
 
 Socket::~Socket()
 {
@@ -12,44 +10,47 @@ Socket::~Socket()
     	cout<<"socket closed"<<endl;
 }
 
-void Socket::Bind()
-{
-		int b=::bind(socketfd, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
-		EXIT_IF(b<0, "bind wrong\n, errno:%d %s",errno, strerror(errno));
-}
-
-void Socket::Listen()
-{
-		int l=listen(socketfd, BACKLOG);
-		EXIT_IF(l<0, "listen wrong\n");
-}
-
-void Socket::initilize_listen()
-{
-		short port=PORT;
-		socketfd=socket(AF_INET, SOCK_STREAM, 0);
-		EXIT_IF(socketfd<0, "socket wrong\n");
-		sock_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-		sock_addr.sin_port=htons(PORT);
-		sock_addr.sin_family=htonl(AF_INET);
-    	Bind();
-    	Listen();
-		set_non_blocking();
-    	cout<<"listening socket bind at: "<<"127.0.0.1:"<<ntohs(sock_addr.sin_port)<<"and keep listening!"<<endl;
-}
-void Socket::set_non_blocking()                                             
+void Socket::set_non_blocking()
 {
         int opts;
         opts = fcntl(socketfd, F_GETFL,0);
         EXIT_IF(opts<0,"fcntl GET wrong\n");
         opts = (opts | O_NONBLOCK);
         EXIT_IF(fcntl(socketfd, F_SETFL, opts) < 0, "fcntl SET wrong\n");
-}     
+}
+
+ListenSocket::ListenSocket(int listenfd, struct sockaddr_in* servaddr):Socket(listenfd, servaddr)
+{
+		cout<<"create a listen socket obj\n";
+}
+void ListenSocket::init()
+{
+		Bind(get_fd(), get_addr());
+		Listen(get_fd());
+    	cout<<"listening socket bind at: "<<"127.0.0.1:"<<ntohs(get_addr()->sin_port)<<"and keep listening!"<<endl;
+}
+void ListenSocket::Bind(int listenfd, struct sockaddr_in* addr)
+{
+		int b=::bind(listenfd, (struct sockaddr*)addr, sizeof(struct sockaddr));
+		EXIT_IF(b<0, "bind wrong\n, errno:%d %s",errno, strerror(errno));
+}
+
+void ListenSocket::Listen(int listenfd)
+{
+		int l=listen(listenfd, BACKLOG);
+		EXIT_IF(l<0, "listen wrong\n");
+}
+
 int main()
 {
-    	class Socket my_socket;
-    	my_socket.initilize_listen();
-    	cout<<"socket init"<<endl;
+		int listenfd=socket(AF_INET, SOCK_STREAM, 0);
+		EXIT_IF(listenfd<0,"socket wrong\n");
+		struct sockaddr_in servaddr;
+		servaddr.sin_port=htons(PORT);
+		servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+		servaddr.sin_family=AF_INET;
+		class ListenSocket listensocket(listenfd, &servaddr);
+		listensocket.init();
 		for(;;)
 		{
 
--- task3_http_server/server.h
+++ task3_http_server/server.h
@@ -20,23 +20,32 @@
 #define READ_SIZE 1024
 
 #define EXIT_IF(r,...) if(r){\
-						printf("__VA_ARGS__");\
-						printf("%s:%d error no:%d msg:%s",__FILE__, __LINE__, errno, strerror(errno));\
+						printf(__VA_ARGS__);\
+						printf("%s:%d\n",__FILE__, __LINE__);\
 						exit(1);}
 class Socket
 {
         private:
-            int socketfd;
-            struct sockaddr_in sock_addr;
-            void Bind();
-            void Listen();
-            void set_non_blocking();
-            void initilize_listen();//bind and listen
+            	int socketfd;
+            	struct sockaddr_in sock_addr;
+				void set_non_blocking();
         public:
-            int get_fd(){return socketfd;};
-            int get_addr(){return sock_addr.sin_addr.s_addr};
-            Socket();
-            ~Socket();
+            	int get_fd(){return socketfd;};
+            	struct sockaddr_in* get_addr(){return &sock_addr;};
+            	Socket();
+				Socket(int fd, struct sockaddr_in* addr);
+            	~Socket();
 };
 
+class ListenSocket:public Socket
+{
+		private:
+				void Bind(int, struct sockaddr_in*);
+				void Listen(int);
+        public:
+				void init();
+				ListenSocket(){};
+				ListenSocket(int, struct sockaddr_in*);
+				~ListenSocket(){};
+};
 #endif
