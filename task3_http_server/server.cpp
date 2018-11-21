155e489d3cd0850ccbad1698b3d3e570a8626088
colinaaa <qingyu.wang@aliyun.com>
2018-11-21 23:52:41 +0000

    feat(http_server): class Epoll almost done
    Epoll without e_write and class ConnectSocket done

--- task3_http_server/server.cpp
+++ task3_http_server/server.cpp
@@ -7,7 +7,8 @@ Socket::Socket(int fd, struct sockaddr_in* addr):socketfd(fd), sock_addr(*addr){
 Socket::~Socket()
 {
     	close(get_fd());
-    	cout<<"socket closed"<<endl;
+		if(TEST_OUTPUT)
+    			cout<<"socket closed"<<endl;
 }
 
 void Socket::set_non_blocking()
@@ -21,7 +22,8 @@ void Socket::set_non_blocking()
 
 ListenSocket::ListenSocket(int listenfd, struct sockaddr_in* servaddr):Socket(listenfd, servaddr)
 {
-		cout<<"create a listen socket obj\n";
+		if(TEST_OUTPUT)
+				cout<<"create a listen socket obj\n";
 }
 void ListenSocket::init()
 {
@@ -32,13 +34,86 @@ void ListenSocket::init()
 void ListenSocket::Bind(int listenfd, struct sockaddr_in* addr)
 {
 		int b=::bind(listenfd, (struct sockaddr*)addr, sizeof(struct sockaddr));
-		EXIT_IF(b<0, "bind wrong\n, errno:%d %s",errno, strerror(errno));
+		EXIT_IF(b<0, "bind wrong\n");
 }
 
 void ListenSocket::Listen(int listenfd)
 {
 		int l=listen(listenfd, BACKLOG);
 		EXIT_IF(l<0, "listen wrong\n");
+}
+
+ConnectSocket::ConnectSocket(int fd, struct sockaddr_in* addr):Socket(fd, addr);
+{
+		if(TEST_OUTPUT)
+				std::cout<<"got connection from"<<ntohl(sock_addr.sin_addr.s_addr)<<":"<<ntohs(sock_addr.sin_port)<<std::endl;
+};
+
+Epoll::Epoll()
+{
+		epollfd=epoll_create(EPOLL_SIZE);
+		maxevs=MAXEVENTS;
+}
+
+void Epoll::event_add(int fd, int state)
+{
+		//struct epoll_event ev;
+		//ev.data.fd=fd;
+		//ev.events=state|EPOLLET;
+		struct epoll_event* ev=fd_ev[fd];
+		ev->events=state|EPOLLET;
+		int r=epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, ev);
+		EXIT_IF(r<0, "event add wrong\n");
+		if(TEST_OUTPUT)
+				cout<<"event added"<<endl;
+}
+
+void Epoll::event_delete(int fd)
+{
+		//struct epoll_event ev;
+		//ev.data.fd=fd;
+		//ev.events=state|EPOLLET;
+		struct epoll_event* ev=fd_ev[fd];
+		int r=epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, ev);
+		EXIT_IF(r<0, "event delete wrong\n");
+		if(TEST_OUTPUT)
+				cout<<"event deleted"<<endl;
+}
+
+void Epoll::event_modify(int fd, int state)
+{
+		//struct epoll_event ev;
+		struct epoll_event* ev=fd_ev[fd];
+		ev->events=state|EPOLLET;
+		int r=epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, ev);
+}
+string* Epoll::e_read(int fd)
+{
+		int nread;
+		int n=0;
+		char buf[MAXBUFFER];
+		size_t nleft=sizeof(buf);
+		while ((nread=read(fd, buf+n, READ_SIZE))>0)
+		{
+				n+=nread;
+				if(TEST_OUTPUT)
+						cout<<"read: "<<nread<<endl;
+		}
+		if(nread==-1&&errno!=EAGAIN)
+		{
+				cout<<"read wrong"<<endl;
+				event_delete(fd, EPOLLIN);
+				close(fd);
+		}
+		else if(nread==0)
+		{
+				cout<<"client close"<<endl;
+				event_delete(fd, EPOLLIN);
+				close(fd);
+		}
+		if(TEST_OUTPUT)
+				printf("receive:%s",buf);
+		//event_modify(fd, EPOLLOUT);在httpser中转换
 }
 
 int main()
--- task3_http_server/server.h
+++ task3_http_server/server.h
@@ -16,12 +16,14 @@
 #define BACKLOG 10
 #define PORT 9999
 #define MAXBUFFER 1024
+#define MAXEVENTS 100 //for epoll
 #define EPOLL_SIZE 4096
 #define READ_SIZE 1024
+#define TEST_OUTPUT 1
 
 #define EXIT_IF(r,...) if(r){\
 						printf(__VA_ARGS__);\
-						printf("%s:%d\n",__FILE__, __LINE__);\
+						printf("%s:%d\n, errno:%d, msg:%s",__FILE__, __LINE__,errno, strerror(errno));\
 						exit(1);}
 class Socket
 {
@@ -49,4 +51,29 @@ class ListenSocket:public Socket
 				ListenSocket(int, struct sockaddr_in*);
 				~ListenSocket(){};
 };
+class ConnectSocket:public Socket
+{
+		public:
+				ConnectSocket(){};
+				ConnectSocket(int, struct sockaddr_in*);
+				~ConnectSocket(){close(socketfd);};
+}
+class Epoll
+{
+		private:
+				int epollfd;
+				int maxevs;
+				struct epoll_event* active_evs;
+				map<int, epoll_event*>fd_ev;
+		public:
+				std::string* e_read(int fd);
+				int e_write(int fd);
+				void event_delete(int, int);
+				void event_modify(int, int);
+				void event_add(int, int);
+				Epoll();
+				~Epoll();
+
+
+}
 #endif
