9374a3fd39e8fdf900707c396e37bb9f47e591fa
colinaaa <qingyu.wang@aliyun.com>
2018-12-13 16:32:05 +0800

    test(conch): config for test

--- task6_backend/conch/config.py
+++ task6_backend/conch/config.py
@@ -5,12 +5,31 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-        basedir, 'data.sqlite')
 
     @staticmethod
     def init_app(app):
         pass
 
 
-config = {'default': Config}
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+        basedir, 'data.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+        basedir, 'data.sqlite')
+
+
+config = {
+    'default': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+}
